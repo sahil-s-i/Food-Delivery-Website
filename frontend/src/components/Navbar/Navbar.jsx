50506f12936b85f637b7b43947898e11d0934ac0
Sahil <sahilismailnavar@gmail.com>
2024-06-18 23:09:50 +0530

    Added code in the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css';
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-
+            
         </div>
     )
 }
