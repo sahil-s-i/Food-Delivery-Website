c289763600e66038870725b6d899b303b547b8c8
Sahil <sahilismailnavar@gmail.com>
2024-06-19 21:19:00 +0530

    Added search icon and basket icon

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,9 @@
 import logo from './logo.png';
+import search_icon from './search_icon.png';
+import basket_icon from './shopping-bag.png';
 
 export const assets = {
     logo,
+    search_icon,
+    basket_icon,
 }
