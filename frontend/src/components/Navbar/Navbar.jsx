171226609b340aea0bc55b7ddbd91787f636d9b1
Sahil <sahilismailnavar@gmail.com>
2024-06-21 22:00:58 +0530

    Added navlinks and nav items

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("home");
+
+
     return (
         <div className='navbar'>
             <img src={assets.logo} alt="logo image" className='logo' />
             <ul className="navbar-menu">
-                <li>home</li>
-                <li>menu</li>
-                <li>mobile-app</li>
-                <li>contact us</li>
+                <li className={menu==="home"?"active":""}>home</li>
+                <li className={menu==="menu"}>menu</li>
+                <li className={menu==="mobile-app"}>mobile-app</li>
+                <li className={menu==="contact us"}>contact us</li>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search icon" className='resize-img'/>
