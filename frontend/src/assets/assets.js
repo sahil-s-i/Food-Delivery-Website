9e2754f49a350cb20125820822fc455c97abdbea
Sahil <sahilismailnavar@gmail.com>
2024-06-24 19:11:23 +0530

    Modified assest.js code

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import search_icon from './search_icon.png';
 import basket_icon from './shopping-bag.png';
+import header_img from './header_img.jpeg';
 
 export const assets = {
     logo,
     search_icon,
     basket_icon,
+    header_img,
 }
