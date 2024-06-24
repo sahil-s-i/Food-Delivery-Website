7163d2557548cdfc244daa1d415830e39e5ee63f
Sahil <sahilismailnavar@gmail.com>
2024-06-24 18:54:19 +0530

    Added h2 and class with the div

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+      </div>
     </div>
   )
 }
