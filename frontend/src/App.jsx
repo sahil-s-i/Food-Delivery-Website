54347a2be5b7319ba167fc0b72950e4781a4d137
Sahil <sahilismailnavar@gmail.com>
2024-06-24 18:41:47 +0530

    Added routers in the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
