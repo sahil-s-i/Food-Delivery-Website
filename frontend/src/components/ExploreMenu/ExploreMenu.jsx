362593831016d958016c6ca63a68f73f5f551999
Sahil <sahilismailnavar@gmail.com>
2024-06-25 21:36:12 +0530

    imported css file in the code

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './ExploreMenu.css'
 
 const ExploreMenu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default ExploreMenu
