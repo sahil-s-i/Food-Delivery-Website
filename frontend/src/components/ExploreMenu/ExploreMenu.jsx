9a270310abbef8a5f04baba5d89796acd60ddbee
Sahil <sahilismailnavar@gmail.com>
2024-06-26 21:43:34 +0530

    Added classnames and paragraph in the code

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,6 +6,10 @@ const ExploreMenu = () => {
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
+            <p className='explore-menu-text'></p>
+            <div className="explore-menu-list">
+                
+            </div>
 
         </div>
     )
