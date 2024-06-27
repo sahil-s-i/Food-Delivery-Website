60d0f443d03be47c90c8d02ebc1e8ce678f2932c
Sahil <sahilismailnavar@gmail.com>
2024-06-27 22:30:00 +0530

    Added menu 1

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,12 +2,15 @@ import logo from './logo.png';
 import search_icon from './search_icon.png';
 import basket_icon from './shopping-bag.png';
 import header_img from './header_img.png';
+import menu_1 from './menu_1.jpg';
 
 export const assets = {
     logo,
     search_icon,
     basket_icon,
     header_img,
+    menu_1,
+
 }
 
 export const menu_list = [
