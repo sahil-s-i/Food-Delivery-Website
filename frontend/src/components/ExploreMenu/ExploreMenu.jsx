de5f60769f899bd57589e99153e4e6b05008faf9
Sahil <sahilismailnavar@gmail.com>
2024-07-01 22:16:17 +0530

    Added description in the exploremenu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = () => {
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
-            <p className='explore-menu-text'></p>
+            <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal to a time</p>
             <div className="explore-menu-list">
                 {menu_list.map((item, index) => {
                     return (
