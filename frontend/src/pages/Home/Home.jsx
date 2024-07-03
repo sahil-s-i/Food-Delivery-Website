a95b62dcd0afeeebaa9ce9eaf5bb1c893db2e3fa
Sahil <sahilismailnavar@gmail.com>
2024-07-03 20:14:40 +0530

    Added category setCategory in the home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
