1c6458e46154bb468cad33e98f27c691ffb59bc8
Sahil <sahilismailnavar@gmail.com>
2024-07-03 20:37:37 +0530

    Added return code in the file

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,6 +3,18 @@ import { createContext } from "react";
 
 export const StoreContext = createContext(null)
 
-const StoreContextProvider = (props) =>{
+const StoreContextProvider = (props) => {
 
+
+
+
+
+    const contextValue = {
+
+    }
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.childern}
+        </StoreContext.Provider>
+    )
 }
