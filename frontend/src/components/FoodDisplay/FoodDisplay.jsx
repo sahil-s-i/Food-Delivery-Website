0c2bd3f4c034940820aa3c36a959c1945314ac3c
Sahil <sahilismailnavar@gmail.com>
2024-07-04 21:26:18 +0530

    Returned food item with parameter

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css';
 import { StoreContext } from '../../context/StoreContext'
+import Fooditem from '../Fooditem/Fooditem';
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext)
@@ -8,8 +9,8 @@ const FoodDisplay = ({ category }) => {
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item,index) => {
-          return 
+        {food_list.map((item, index) => {
+          return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
 
         })}
       </div>
