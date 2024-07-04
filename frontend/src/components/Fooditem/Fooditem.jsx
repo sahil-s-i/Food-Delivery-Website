d08c4ef6520b5ff9a0e5dbb16a5b8e65b67ce7c5
Sahil <sahilismailnavar@gmail.com>
2024-07-04 21:32:27 +0530

    Added image in the code

--- frontend/src/components/Fooditem/Fooditem.jsx
+++ frontend/src/components/Fooditem/Fooditem.jsx
@@ -3,7 +3,10 @@ import './Fooditem.css';
 
 const Fooditem = ({id,name,price,description,image}) => {
   return (
-    <div>
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            <img className='food-item-image' src={image} alt="food item image" />
+        </div>
       
     </div>
   )
