06e40da5568f41ceddb269a05239b089df9f09e6
Sahil <sahilismailnavar@gmail.com>
2024-07-05 22:19:45 +0530

    Added multiple divs in the code

--- frontend/src/components/Fooditem/Fooditem.jsx
+++ frontend/src/components/Fooditem/Fooditem.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Fooditem.css';
 
-const Fooditem = ({id,name,price,description,image}) => {
+const Fooditem = ({ id, name, price, description, image }) => {
   return (
     <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={image} alt="food item image" />
+      <div className="food-item-img-container">
+        <img className='food-item-image' src={image} alt="food item image" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+
         </div>
-      
+      </div>
     </div>
   )
 }
