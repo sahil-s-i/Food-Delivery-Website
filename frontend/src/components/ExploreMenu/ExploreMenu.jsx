d41b57ee370b702a29162e121657f1585a2f7964
Sahil <sahilismailnavar@gmail.com>
2024-07-08 18:53:18 +0530

    Edited code file

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -11,7 +11,7 @@ const ExploreMenu = ({ category, setCategory }) => {
                 {menu_list.map((item, index) => {
                     return (
                         <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
-                            <img className={category === item.menu_name ? "active 'image-resize'" : "image-resize"} src={item.menu_image} alt="menu image" />
+                            <img className={category === item.menu_name ? "active image-resize" : "image-resize"} src={item.menu_image} alt="menu image" />
                             <p>{item.menu_name}</p>
                         </div>
                     )
