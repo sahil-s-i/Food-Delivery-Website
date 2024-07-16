613180481d96ca9b3a69b6df1d79cc5f101fd913
Sahil <sahilismailnavar@gmail.com>
2024-07-16 21:49:45 +0530

    Added social icon in code

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,7 +9,11 @@ const Footer = () => {
         <div className="footer-content-left">
           <img src={assets.basket_icon} alt="logo image" />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus quaerat error ex, atque eaque nulla vel, ut magnam unde velit alias consequatur et dicta harum doloremque, dolor voluptates recusandae!</p>
-          
+          <div className="footer-social-icons">
+            <img src={assets.add_item} alt="facebook icon" />
+            <img src={assets.add_item} alt="twitter icon" />
+            <img src={assets.add_item} alt="linked icon" />
+          </div>
         </div>
         <div className="footer-content-center">
 
