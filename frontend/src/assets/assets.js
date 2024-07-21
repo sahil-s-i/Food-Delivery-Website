c79ed155999a888efca3db686cb5981febacca61
Sahil <sahilismailnavar@gmail.com>
2024-07-21 18:36:50 +0530

    Added play store image icon

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -10,6 +10,7 @@ import facebook from './facebook.png';
 import twitter from './twitter.png';
 import linkedin from './linkedin.png';
 import app_store from './app_store.png';
+import play_store from './play_store.png';
 
 
 import menu_1 from './menu_1.jpeg';
@@ -68,6 +69,7 @@ export const assets = {
     twitter,
     linkedin,
     app_store,
+    play_store,
 }
 
 export const menu_list = [
