66b31b9cc7a640a622ec0a41413cad930dd23cc7
Sahil <sahilismailnavar@gmail.com>
2024-07-24 22:41:30 +0530

    Added setShowLogin in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("menu");
 
