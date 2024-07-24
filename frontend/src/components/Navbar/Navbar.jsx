940be65ce96d5e3ff9da4b9e281a99a691d34ff5
Sahil <sahilismailnavar@gmail.com>
2024-07-24 22:09:27 +0530

    Added anchor tag href

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
             </div>
             <ul className="navbar-menu">
                 <Link onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
-                <a onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
-                <a onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
-                <a onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
+                <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+                <a href='#' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
+                <a href='#' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search icon" className='resize-img' />
