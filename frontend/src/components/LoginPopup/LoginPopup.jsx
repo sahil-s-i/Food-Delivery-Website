01e569aa933db851d0e93d19c3d6e22440ced36f
Sahil <sahilismailnavar@gmail.com>
2024-07-25 21:28:13 +0530

    Added useState in code

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
 
 const LoginPopup = () => {
+
+    const [currState, setCurrState] = useState("Sign up");
+
     return (
         <div className='login-popup'>
             <form className="login-popup-container">
                 <div className="login-popup-title">
-                    
+                    <h2>{currState}</h2>
                 </div>
             </form>
         </div>
