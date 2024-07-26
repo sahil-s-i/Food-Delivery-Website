71cd8c258df03d8ea56b0f07579bb4a9e9e65004
Sahil <sahilismailnavar@gmail.com>
2024-07-26 22:40:56 +0530

    Added onclick on span tag

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -27,7 +27,7 @@ const LoginPopup = ({ setShowLogin }) => {
                 </div>
                 {currState === "Login" ?
                     <p>Create a new account ? <span onClick={()=>setCurrState("Sign up")}>Click here</span></p> :
-                    <p>Already have an account ? <span onClick={()=>setCurrState("Login")}>Login here</span></p>}
+                    <p>Already have an account ? <span onClick={()=>setCurrState("Login ")}>Login here</span></p>}
 
 
             </form>
