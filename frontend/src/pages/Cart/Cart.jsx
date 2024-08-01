0b871295ac2bff3b9e152c7c26d37de64805d4e1
Sahil <sahilismailnavar@gmail.com>
2024-08-01 21:34:43 +0530

    Added useContext in code

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart, } = useContext(StoreContext);
   return (
     <div>
-      
+
     </div>
   )
 }
