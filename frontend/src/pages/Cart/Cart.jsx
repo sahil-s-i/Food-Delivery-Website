4fbfca4fe84c7204605698385072ffea0ee8c32b
Sahil <sahilismailnavar@gmail.com>
2024-08-03 20:40:17 +0530

    Added classname for x

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -28,7 +28,7 @@ const Cart = () => {
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price * cartItems[item._id]}</p>
-                  <p>x</p>
+                  <p className='cross'>x</p>
                 </div>
                 <hr />
               </div>
