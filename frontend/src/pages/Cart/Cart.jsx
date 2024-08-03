388d344f02eadf515c5a25461cbdcf585ec5f26b
Sahil <sahilismailnavar@gmail.com>
2024-08-03 21:01:00 +0530

    Modified the code to calculate the total amount

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -27,13 +27,12 @@ const Cart = () => {
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>${parseFloat(item.price.replace('$', '')) * cartItems[item._id]}</p>
                   <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
                 </div>
                 <hr />
               </div>
             )
-
           }
         })}
       </div>
