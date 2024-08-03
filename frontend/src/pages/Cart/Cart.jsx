db044a609825d4d8f60bb3e7d4cfde1588ebe432
Sahil <sahilismailnavar@gmail.com>
2024-08-03 20:29:43 +0530

    Added div for returning

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -20,14 +20,17 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div className="cart-items-title cart-items-item">
-                {/* <p>{item.name}</p> */}
-                <img src={item.image} alt="item image" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price * cartItems[item._id]}</p>
-                <p>x</p>
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  {/* <p>{item.name}</p> */}
+                  <img src={item.image} alt="item image" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>x</p>
+                </div>
+                <hr />
               </div>
             )
 
