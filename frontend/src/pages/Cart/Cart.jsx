826840033093ad5fb026a76f4ae35e5fe0a9af61
Sahil <sahilismailnavar@gmail.com>
2024-08-03 20:45:41 +0530

    Added removefromcart function to x through onclick

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -28,7 +28,7 @@ const Cart = () => {
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price * cartItems[item._id]}</p>
-                  <p className='cross'>x</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
                 </div>
                 <hr />
               </div>
