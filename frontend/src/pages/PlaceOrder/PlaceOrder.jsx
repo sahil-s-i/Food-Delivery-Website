33ec6e948ebb0b19735988c0b2e8464f4a7fe122
Sahil <sahilismailnavar@gmail.com>
2024-08-07 20:14:53 +0530

    Added left and right div in code segment

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,9 +3,14 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
   )
 }
 
