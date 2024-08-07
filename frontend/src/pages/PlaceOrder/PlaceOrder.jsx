f0ee15d44eba4a5d2d69cb8d56152110f815d77a
Sahil <sahilismailnavar@gmail.com>
2024-08-07 20:18:09 +0530

    Added p tag with content and div with fields in code segment

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -5,7 +5,11 @@ const PlaceOrder = () => {
   return (
     <form className='place-order'>
       <div className="place-order-left">
-
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
+        </div>
       </div>
       <div className="place-order-right">
 
