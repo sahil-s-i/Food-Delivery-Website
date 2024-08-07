6a7cb10b1acecf426d8cb3e588e48b50842ae85d
Sahil <sahilismailnavar@gmail.com>
2024-08-07 20:30:12 +0530

    Added getTotalCartAmount and useContext in code

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -20,10 +24,29 @@ const PlaceOrder = () => {
           <input type="text" placeholder='Zip code' />
           <input type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder='Phone No.'/>
+        <input type="text" placeholder='Phone No.' />
       </div>
       <div className="place-order-right">
-
+        <div className="cart-total">
+          <h2>cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Charge</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
       </div>
     </form>
   )
