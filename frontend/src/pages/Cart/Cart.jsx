6c2b83e76df32328179928785f8b667f09765d14
Sahil <sahilismailnavar@gmail.com>
2024-08-07 19:53:36 +0530

    imported useNavigate from react-router-dom

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
