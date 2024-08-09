4c4d21c8e0fada429e0888152937ad6cac14c05f
Sahil <sahilismailnavar@gmail.com>
2024-08-09 10:36:25 +0530

    Added server.js file with app config

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+
+// app config 
+const app = express();
+const PORT = 4000;
+
+// middle ware 
+app.use(express.json())
+app.use(cors)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
