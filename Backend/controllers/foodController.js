ab0f78dbfbe62979dcd484397cc75819671d4634
Sahil <sahilismailnavar@gmail.com>
2024-08-11 13:20:29 +0530

    Added data from req in add food func

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -3,6 +3,14 @@ import fs from "fs";
 
 // Add food item 
 const addFood = async((req, res) => {
+    let image_filename = `${req.file.filename}`;
+
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+    })
 
 })
 
