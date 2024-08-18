be3fb946cd405633a335653c8306f918cad54366
Sahil <sahilismailnavar@gmail.com>
2024-08-18 16:09:41 +0530

    Added code lines for listfood try catch

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -37,7 +37,8 @@ const listfood = async (req, res) => {
         const foods = await foodModel.find({});
         res.json({ success: true, data: foods })
     } catch (error) {
-
+        console.log(error);
+        res.json({ success: false, message: "Error" })
     }
 }
 
