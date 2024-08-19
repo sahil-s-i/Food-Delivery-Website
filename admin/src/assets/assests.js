b8fdc3df8b2e80a4303ec73a008c09135116a4b0
Sahil <sahilismailnavar@gmail.com>
2024-08-19 17:17:40 +0530

    Added profile_image image code and exported image

--- admin/src/assets/assests.js
+++ admin/src/assets/assests.js
@@ -1,8 +1,10 @@
 import add_icon from './add.png';
 import order_icon from './order-confirmed.png';
+import profile_image from './user.png';
 
 export const assets = {
     add_icon,
     order_icon,
+    profile_image,
     
 }
