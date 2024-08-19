fc6a7030595a3170e6a51e18175ddfae96c81180
Sahil <sahilismailnavar@gmail.com>
2024-08-19 17:18:44 +0530

    Added upload image code and exported image

--- admin/src/assets/assests.js
+++ admin/src/assets/assests.js
@@ -1,10 +1,12 @@
 import add_icon from './add.png';
 import order_icon from './order-confirmed.png';
 import profile_image from './user.png';
+import upload from './uploading.png';
 
 export const assets = {
     add_icon,
     order_icon,
     profile_image,
+    upload,
     
 }
