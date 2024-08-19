0ae7d146613f8d9a1a0c40dd0137196ab3ffb80f
Sahil <sahilismailnavar@gmail.com>
2024-08-19 17:20:27 +0530

    Added parcel_icon image code and exported image

--- admin/src/assets/assests.js
+++ admin/src/assets/assests.js
@@ -2,11 +2,12 @@ import add_icon from './add.png';
 import order_icon from './order-confirmed.png';
 import profile_image from './user.png';
 import upload from './uploading.png';
+import parcel_icon from './parcel.png';
 
 export const assets = {
     add_icon,
     order_icon,
     profile_image,
     upload,
-    
+    parcel_icon
 }
