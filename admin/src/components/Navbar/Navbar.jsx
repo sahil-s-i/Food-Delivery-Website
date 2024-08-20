60fbc71eee5e73b5e699ff5e3955ab4237ffe896
Sahil <sahilismailnavar@gmail.com>
2024-08-20 19:36:33 +0530

    Added code in jsx file

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Navbar
