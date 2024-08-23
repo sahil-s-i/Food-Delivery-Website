2386ea32471cc4c74640c4766c8bd0e8bff1ce01
Sahil <sahilismailnavar@gmail.com>
2024-08-23 19:36:05 +0530

    Added multiple divs with the icons

--- admin/src/components/Sidebar/SIdebar.jsx
+++ admin/src/components/Sidebar/SIdebar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Sidebar.css';
+import { assets } from '../../assets/assests';
 
 const SIdebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
         <div className="sidebar-option">
-
+          <img src={assets.add_icon} alt="Add icon" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="Add icon" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="Add icon" />
+          <p>Add Items</p>
         </div>
       </div>
     </div>
