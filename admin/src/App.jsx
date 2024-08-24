9eff493200d773269055b421c3fef119346ec98f
Sahil <sahilismailnavar@gmail.com>
2024-08-24 14:38:58 +0530

    Added route for orders component in pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar/SIdebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path='/add' element={<Add />} />
           <Route path='/list' element={<List />} />
+          <Route path='/orders' element={<Orders />} />
         </Routes>
       </div>
     </div>
