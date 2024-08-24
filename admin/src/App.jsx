3d311a42f66561fcdf2ae50d10aa78272d1bba22
Sahil <sahilismailnavar@gmail.com>
2024-08-24 14:34:49 +0530

    Added react router dom routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/SIdebar'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -9,7 +10,9 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
-
+        <Routes>
+          
+        </Routes>
       </div>
     </div>
   )
