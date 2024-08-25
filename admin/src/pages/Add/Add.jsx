37bab40ef6d7bed4546ea1cd6b501f7da8b006de
Sahil <sahilismailnavar@gmail.com>
2024-08-25 21:30:51 +0530

    Added img to uplaod image

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Add.css';
+import { assets } from '../../assets/assests'
 
 const Add = () => {
     return (
         <div className='Add'>
             <form className='flex-col'>
                 <div className="add-img-upload flex-col">
-
+                    <p>Upload Image</p>
+                    <label htmlFor="image">
+                        <img src={assets.upload} alt="image upload" />
+                    </label>
                 </div>
             </form>
         </div>
