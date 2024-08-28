665df4dbae9bb9e3171335630337814e22c0bfc0
Sahil <sahilismailnavar@gmail.com>
2024-08-28 16:44:47 +0530

    Added check for success and error using toast

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './List.css';
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 const List = () => {
 
@@ -11,6 +12,9 @@ const List = () => {
         const response = await axios.get(`${url}/api/food/list`)
         if (response.data.success) {
             setList(response.data.data)
+        }
+        else {
+            toast.error("Error")
         }
     }
     return (
