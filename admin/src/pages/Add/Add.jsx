92e4203431afddacbc84bfbdf461e00f5032e5f2
Sahil <sahilismailnavar@gmail.com>
2024-08-28 16:18:54 +0530

    Added check condition for image and data wheather stored or not

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -28,6 +28,18 @@ const Add = () => {
         formData.append("category", data.category)
         formData.append("image", image)
         const response = await axios.post(`${url}/api/food/add`, formData)
+        if (response.data.success) {
+             setData({
+                name: "",
+                description: "",
+                price: "",
+                category: "Salad"
+            })
+            setImage(false)
+        }
+        else{
+
+        }
     }
 
     return (
