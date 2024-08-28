e78ff4be8d20645f7c20ed79b7b938d7b0789123
Sahil <sahilismailnavar@gmail.com>
2024-08-28 16:43:00 +0530

    Added url with fetchlist method in the code

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './List.css';
+import axios from "axios";
 
 const List = () => {
+
+    const url = "http://localhost:4000"
+    const [list, setList] = useState([]);
+
+    const fetchList = async () => {
+        const response = await axios.get(`${url}/api/food/list`)
+        if (response.data.success) {
+            setList(response.data.data)
+        }
+    }
     return (
         <div>
 
