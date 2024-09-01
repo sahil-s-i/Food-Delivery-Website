9afec6635784ca39f6590022354175297a4fc0e9
Sahil <sahilismailnavar@gmail.com>
2024-09-01 16:42:32 +0530

    Added loginuser and register func in the file

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,16 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+
+
+//login user
+const loginUser = async (req, res) => {
+
+}
+
+//Register user
+const registerUser = async (req, res) => {
+
+}
 
