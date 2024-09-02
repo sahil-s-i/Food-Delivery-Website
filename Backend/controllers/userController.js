f6aabcbfa33b9cb79fc2b27ff7eb2abca461fe67
Sahil <sahilismailnavar@gmail.com>
2024-09-02 22:44:23 +0530

    Added code functionality for registerUser

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -11,7 +11,16 @@ const loginUser = async (req, res) => {
 
 //Register user
 const registerUser = async (req, res) => {
+    const { name, password, email } = req.body;
+    try {
+        const exist = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" })
 
+        }
+    } catch (error) {
+
+    }
 }
 
 export { loginUser, registerUser }
