f9ed647e844169a93ff2e0551b018f393393111f
Sahil <sahilismailnavar@gmail.com>
2024-09-02 22:59:12 +0530

    Added code for newuser obj usermodel

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -32,7 +32,11 @@ const registerUser = async (req, res) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword
+        })
 
     } catch (error) {
 
