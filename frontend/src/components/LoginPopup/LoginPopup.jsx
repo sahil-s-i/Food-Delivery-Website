cba770a8b02bfd3aa1b52a023a14bbe6b308f3db
Sahil <sahilismailnavar@gmail.com>
2024-09-05 22:57:16 +0530

    used state variable to store pass and name

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -14,6 +14,7 @@ const LoginPopup = ({ setShowLogin }) => {
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }))
     }
 
     return (
