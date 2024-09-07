b3e91f2ec7fc4b7eea3a9b6a7ca23e937e8e3cdf
Sahil <sahilismailnavar@gmail.com>
2024-09-07 22:14:40 +0530

    Added code lines of set token and other

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -35,7 +35,7 @@ const LoginPopup = ({ setShowLogin }) => {
 
         if (response.data.success) {
             setToken(response.data.token);
-            localStorage.setItem("token", response.data.token)
+            localStorage.setItem("token", response.data.token);
         }
     }
 
