47df5de9218492b3ee16a32f7a409601ea294525
Sahil <sahilismailnavar@gmail.com>
2024-09-14 23:32:49 +0530

    Modified code lines

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
-
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
