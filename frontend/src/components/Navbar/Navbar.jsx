f0c3cc6f3136cc70055c8a7dae90ecae54d6a320
Sahil <sahilismailnavar@gmail.com>
2024-09-26 21:58:45 +0530

    modified code lines

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({ setShowLogin }) => {
 
-    
     const [menu, setMenu] = useState("menu");
 
     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
