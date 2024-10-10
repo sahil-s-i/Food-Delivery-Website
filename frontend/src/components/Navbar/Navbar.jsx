6ad7d313908eab0820b5dd0d2018d121ea73e150
Sahil <sahilismailnavar@gmail.com>
2024-10-10 10:53:44 +0530

    modified code lines

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({ setShowLogin }) => {
+    
     const [menu, setMenu] = useState("menu");
 
     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
