f9caad90f0ead5e7d6f38262a2deeb93ad042366
Sahil <sahilismailnavar@gmail.com>
2024-10-20 23:15:33 +0530

    Added catch code while catching the error

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -47,6 +47,7 @@ const removeFood = async (req, res) => {
     try {
         const food = await foodModel.findById(req.body.id);
         fs.unlink(`uploads/${food.image}`, () => { })
+        
 
         await foodModel.findByIdAndDelete(req.body.id);
         res.json({ success: true, message: "Food Removed" });
