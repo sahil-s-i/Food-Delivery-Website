cee288bc7909b4b1d7351e9b59e1e561ca0e6b2f
Sahil <sahilismailnavar@gmail.com>
2024-10-22 16:32:06 +0530

    Structured code lines

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/SIdebar'
-import { Route, Routes } from 'react-router-dom'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
 import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
