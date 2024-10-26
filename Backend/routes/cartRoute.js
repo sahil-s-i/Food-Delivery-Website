874056fb84763ede025784fab79732c482871445
Sahil <sahilismailnavar@gmail.com>
2024-10-26 10:12:38 +0530

    Added middleware in the code

--- Backend/routes/cartRoute.js
+++ Backend/routes/cartRoute.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { addToCart, removeFromCart, getCart } from "../controllers/cartController"
-
+import authMiddleware from "../middleware/auth";
 const cartRouter = express.Router();
 
-cartRouter.post("/add", addToCart )
-cartRouter.post("/remove", removeFromCart )
-cartRouter.post("/get", getCart )
+cartRouter.post("/add", authMiddleware, addToCart)
+cartRouter.post("/remove", authMiddleware, removeFromCart)
+cartRouter.post("/get", authMiddleware, getCart)
 
 export default cartRouter;
 
