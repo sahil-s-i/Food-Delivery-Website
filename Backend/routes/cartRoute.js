1fc3ac957733b8904277213b20250dde7ebef644
Sahil <sahilismailnavar@gmail.com>
2024-10-26 10:05:12 +0530

    Added express routes with api endpoints

--- Backend/routes/cartRoute.js
+++ Backend/routes/cartRoute.js
@@ -1,2 +1,9 @@
 import express from "express"
 import { addToCart, removeFromCart, getCart } from "../controllers/cartController"
+
+const cartRouter = express.Router();
+
+cartRouter.post("/add", addToCart )
+cartRouter.post("/remove", removeFromCart )
+cartRouter.post("/get", getCart )
+
