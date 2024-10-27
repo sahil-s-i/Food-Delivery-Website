d6981256d8c6e629c2ba64d4d6a5f505799bb155
Sahil <sahilismailnavar@gmail.com>
2024-10-27 21:20:27 +0530

    Added cartRoutes api end point in server file

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 const authMiddleware = async (req, res, next) => {
     const { token } = req.headers;
     if (!token) {
-        return res.json({ success: false, message: "Not Authorized Login Agian" })
+        return res.json({ success: false, message: "Not Authorized Login Agian" });
     }
     try {
         const token_Decode = jwt.verify(token, process.env.JWT_SECRET);
