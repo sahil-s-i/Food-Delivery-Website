ddabfc3a695ba33aa25ed643d033e912c234556d
Sahil <sahilismailnavar@gmail.com>
2024-10-28 22:48:28 +0530

    Added try catch blck body with cartdata

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,10 +3,16 @@ import userModel from "../models/userModel.js";
 // add items to user cart
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findOne({_id : req.body.userId});
+        let userData = await userModel.findOne({ _id: req.body.userId });
         let cartData = await userData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        }
+        else {
+            cartData[req.body.itemId] += 1;
+        }
     } catch (error) {
-        
+
     }
 }
 
@@ -16,8 +22,8 @@ const removeFromCart = async (req, res) => {
 }
 
 // fetch user cart data
-const getCart = async (req,res) =>{
+const getCart = async (req, res) => {
 
 }
 
-export {addToCart , removeFromCart , getCart}
+export { addToCart, removeFromCart, getCart }
