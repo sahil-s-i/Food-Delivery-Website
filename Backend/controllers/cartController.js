467cf466418d5f19eaa4ba4988b7c743774b7c02
Sahil <sahilismailnavar@gmail.com>
2024-10-29 22:56:33 +0530

    modified code lines for finding userId in mongoDB

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js";
 // add items to user cart
 const addToCart = async (req, res) => {
     try {
-        let userData = await userModel.findOne({ _id: req.body.userId });
+        let userData = await userModel.findById(req.body.userId);
         let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
@@ -21,7 +21,12 @@ const addToCart = async (req, res) => {
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {
+    try {
+        let userData = await userModel.findById(req.body.userId);
 
+    } catch (error) {
+
+    }
 }
 
 // fetch user cart data
