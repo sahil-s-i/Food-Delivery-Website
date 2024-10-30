15c256d6d304b3028c07b37a469e448aa07cb246
Sahil <sahilismailnavar@gmail.com>
2024-10-30 09:46:43 +0530

    cmplted code for getCart method

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -40,9 +40,10 @@ const getCart = async (req, res) => {
     try {
         let userData = await userModel.findById(req.body.userId);
         let cartData = await userData.cartData;
-        
+        res.json({ success: true, cartData })
     } catch (error) {
-        
+        console.log(error);
+        res.json({ success: false, message: "Error" })
     }
 }
 
