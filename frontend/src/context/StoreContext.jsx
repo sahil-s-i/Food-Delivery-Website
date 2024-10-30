a354743635059b3e5e0c93e53a605f0165f855a5
Sahil <sahilismailnavar@gmail.com>
2024-10-30 23:37:03 +0530

    Added code to add cart data in the DB

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -11,12 +11,15 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
         else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+        if (token) {
+            await axios.post(url+"/api/cart/add", { itemId }, { headers: { token } })
         }
     }
 
