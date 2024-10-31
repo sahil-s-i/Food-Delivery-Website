035e1923883365904ef8fcd9c200d288dbedf12c
Sahil <sahilismailnavar@gmail.com>
2024-10-31 10:00:07 +0530

    Added loadcart data to keep the cart data when page refreshed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -46,6 +46,11 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(url + "/api/cart/get", {}, { headers: { token } });
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(() => {
         async function loadData() {
             await fetchFoodList();
