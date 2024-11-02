a7725be4da7e1755574bf5ba1620beb6465b2bf3
Sahil <sahilismailnavar@gmail.com>
2024-11-02 11:04:34 +0530

    Added placeorder func

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,6 +1,7 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import stripe from "stripe";
+
 // placing user order from frontend 
 const placeOrder = async (req, res) => {
     
