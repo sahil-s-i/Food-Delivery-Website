8289b154db33fd1abeb0877387e13379d158fc32
Sahil <sahilismailnavar@gmail.com>
2024-11-03 23:21:46 +0530

    created stripe variable

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -2,6 +2,8 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
 // placing user order from frontend 
 const placeOrder = async (req, res) => {
     
