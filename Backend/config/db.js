3029bc81b73fcb66152ce0adfdd8324e826936f5
Sahil <sahilismailnavar@gmail.com>
2024-11-03 23:33:17 +0530

    Added catch block if connection failed while connecting to db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,5 +3,8 @@ import mongoose, { connect } from "mongoose";
 export const connectDB = async () => {
     await mongoose.connect('').then(() => {
         console.log('DB connected');
+    }).catch((error) => {
+        console.log(error);
+        console.log('DB connection failed');
     })
 }
