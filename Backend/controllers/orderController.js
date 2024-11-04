394b6e785a333a12456a25e1ebb246a56cbada85
Sahil <sahilismailnavar@gmail.com>
2024-11-04 11:08:51 +0530

    Added try-catch in placeOrder

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -6,7 +6,16 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order from frontend 
 const placeOrder = async (req, res) => {
-    
+    try {
+        const newOrder = new orderModel({
+            userId : req.body.userId,
+            items : req.body.items,
+            amount : req.body.amount,
+            
+        })
+    } catch (error) {
+        
+    }
 }
 
 export { placeOrder };
