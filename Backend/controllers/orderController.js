28d80f8487da1d9e1df1814401ec38edc736f52d
Sahil <sahilismailnavar@gmail.com>
2024-11-04 23:47:49 +0530

    Added Success and cancel url in te code session func

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -40,10 +40,12 @@ const placeOrder = async (req, res) => {
         })
 
         const session = await stripe.checkout.sessions.create({
-            line_items:line_items,
-            mode:"payment",
-            success_url:``
+            line_items: line_items,
+            mode: "payment",
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
         })
+        
     } catch (error) {
 
     }
