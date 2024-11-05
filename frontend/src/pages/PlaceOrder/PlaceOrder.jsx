12cd3dcb726ad5911b71b1653e7c37e173b11b1b
Sahil <sahilismailnavar@gmail.com>
2024-11-05 11:08:05 +0530

    Added onchng and value property in lastname

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -31,7 +31,7 @@ const PlaceOrder = () => {
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
           <input name='firstname' onChange={onChnageHandler} value={data.firstname} type="text" placeholder='First Name' />
-          <input name='' type="text" placeholder='Last Name' />
+          <input name='lastname' onChange={onChnageHandler} value={data.lastname} type="text" placeholder='Last Name' />
         </div>
         <input type="email" placeholder='Email Address' />
         <input type="text" placeholder='Street' />
