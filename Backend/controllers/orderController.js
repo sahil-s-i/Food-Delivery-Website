2a7fac4007b2af7169a7774847750db786a1c418
Sahil <sahilismailnavar@gmail.com>
2024-11-07 10:59:00 +0530

    Added tyr code for find id and update orderController

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -56,7 +56,11 @@ const placeOrder = async (req, res) => {
 const verifyOrder = async (req, res) => {
     const { orderId, success } = req.body;
     try {
-        if (success=="true") {
+        if (success == "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" });
+        }
+        else {
             
         }
     } catch (error) {
