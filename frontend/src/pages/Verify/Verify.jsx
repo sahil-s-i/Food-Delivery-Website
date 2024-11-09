bcfc6e998af634c73441ba0cf20f40c6eb81238c
Sahil <sahilismailnavar@gmail.com>
2024-11-09 11:56:25 +0530

    Added Verify func with the nevigation

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Verify.css';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const Verify = () => {
 
@@ -9,7 +10,21 @@ const Verify = () => {
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
   const { url } = useContext(StoreContext);
-  // console.log(success, orderId)
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", { success, orderId });
+    if (response.data.success) {
+      navigate("/myorders");
+    }
+    else {
+      navigate("/")
+    }
+  }
+
+  useEffect(() => {
+    verifyPayment();
+  }, [])
 
   return (
     <div className='verify'>
