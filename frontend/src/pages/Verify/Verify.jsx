baaf118c5cf61b67e43c4dd693ded0c99f4ed1d7
Sahil <sahilismailnavar@gmail.com>
2024-11-09 11:35:49 +0530

    Added search params in code

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Verify.css';
+import { useSearchParams } from 'react-router-dom';
 
 const Verify = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  
   return (
     <div>
-      
+
     </div>
   )
 }
