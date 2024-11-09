8f2520d96bd36f707a75ed72bb253907f5798751
Sahil <sahilismailnavar@gmail.com>
2024-11-09 11:42:40 +0530

    Added url from store context

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Verify.css';
 import { useSearchParams } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 const Verify = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
-
+  const {url} = useContext(StoreContext);
   // console.log(success, orderId)
 
   return (
