e61b810ea8a6670978649ad37421bbc7e715f070
Sahil <sahilismailnavar@gmail.com>
2024-11-10 16:10:29 +0530

    Added user Orders func code

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -73,7 +73,13 @@ const verifyOrder = async (req, res) => {
 
 //user orders for frontend
 const userOrders = async (req, res) => {
-    
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
 }
 
-export { placeOrder, verifyOrder , userOrders };
+export { placeOrder, verifyOrder, userOrders };
