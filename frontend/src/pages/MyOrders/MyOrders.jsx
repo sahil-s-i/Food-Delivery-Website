331c5db98f0be046548cb6547d34fcda159b2238
Sahil <sahilismailnavar@gmail.com>
2024-11-10 16:31:30 +0530

    Added storecontext and also usestate variables

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './MyOrders.css'
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const MyOrders = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const { url, token } = useContext(StoreContext);
+    const [data, setData] = useState([]);
+
+    return (
+        <div>
+
+
+        </div>
+    )
 }
 
 export default MyOrders
