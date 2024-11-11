9231b7155a495ad1e9ef9b936ac88ce74270f851
Sahil <sahilismailnavar@gmail.com>
2024-11-11 11:54:50 +0530

    Imported useNavigate and add component to ues

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const PlaceOrder = () => {
 
@@ -51,6 +52,8 @@ const PlaceOrder = () => {
     }
   }
 
+  const navigate = useNavigate();
+  
   useEffect(()=>{
     if(!token){
       navigate('/cart');
