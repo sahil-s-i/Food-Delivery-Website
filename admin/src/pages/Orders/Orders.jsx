d159a13af9df48520398d45a9b738bb6c1f5dc3f
Sahil <sahilismailnavar@gmail.com>
2024-11-12 10:36:26 +0530

    Adde code lines in Orders func

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import './Orders.css';
+import axios from 'axios';
 
-const Orders = ({url}) => {
+const Orders = ({ url }) => {
     const [orders, setOrders] = useState([]);
 
-    const fetchAllOrders = async () =>{
-        
+    const fetchAllOrders = async () => {
+        const response = await axios.get(url + "/api/order/list");
+        if (response.data.success) {
+            setOrders(response.data.data);
+            console.log(response.data.data)
+        }
     }
 
     return (
