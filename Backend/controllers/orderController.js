b452e661a9d1fcf73e34a8b396a9c157a59a32b4
Sahil <sahilismailnavar@gmail.com>
2024-11-14 23:06:01 +0530

    Changed code in listOrderes

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -88,7 +88,6 @@ const listOrders = async (req, res) => {
         const orders = await orderModel.find({});
         res.json({ success: true, data: orders })
     } catch (error) {
-        
         console.log(error);
         res.json({ success: false, message: "Error" })
     }
