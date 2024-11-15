8e1fe5f67cdca0e4cf3b168bb40bf44f4ea4222a
Sahil <sahilismailnavar@gmail.com>
2024-11-15 23:27:29 +0530

    Added code in return of the order.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -24,35 +24,39 @@ const Orders = ({ url }) => {
 
     return (
         <div className='order add'>
-            {orders.map((order, index) => {
-                <div key={index} className="order-item">
-                    <img src={assets.parcel_icon} alt="Parcel icon" />
-                    <div>
-                        <p className='order-item-food'>
-                            {order.item.map((item, index) => {
-                                if (index === order.item.length - 1) {
-                                    return item.name + " x " + item.quantity;
-                                }
-                                else {
-                                    return item.name + " x " + item.quantity + ", ";
-                                }
-                            })}
-                        </p>
-                        <p className="order-item-name">
-                            {order.address.firstname + " " + order.address.lastname}
-                        </p>
-                        <div className='order-item-address'>
-                            <p>{order.address.street+","}</p>
-                            <p>{order.address.city+", " + order.address.state+", " + order.address.country+", "+order.address.zipcode }</p>
+            <h3>Order page</h3>
+            <div className="order-list">
+                {/* <p>{orders.length}</p> */}
+                {orders.map((order, index) => {
+                    <div key={index} className="order-item">
+                        <img src={assets.parcel_icon} alt="Parcel icon" />
+                        <div>
+                            <p className='order-item-food'>
+                                {order.items.map((item, index) => {
+                                    if (index === order.items.length - 1) {
+                                        return item.name + " x " + item.quantity;
+                                    }
+                                    else {
+                                        return item.name + " x " + item.quantity + ", ";
+                                    }
+                                })}
+                            </p>
+                            <p className="order-item-name">
+                                {order.address.firstname + " " + order.address.lastname}
+                            </p>
+                            <div className='order-item-address'>
+                                <p>{order.address.street + ","}</p>
+                                <p>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</p>
+                            </div>
+                            <p className='order-item-phone'>
+                                {order.address.phone}
+                            </p>
                         </div>
-                        <p className='order-item-phone'>
-                            {order.address.phone}
-                        </p>
+                        <p>Items : {order.items.length}</p>
+                        <p>${order.amount}</p>
                     </div>
-                    <p>Items : {order.item.length}</p>
-                    <p>${order.amount}</p>
-                </div>
-            })}
+                })}
+            </div>
         </div>
     )
 }
