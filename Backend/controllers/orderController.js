faa09ca0e037e1145e67bd2047c826f2817aca4a
Sahil <sahilismailnavar@gmail.com>
2024-11-15 23:23:03 +0530

    Changed code in listOrderes

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -16,6 +16,7 @@ const placeOrder = async (req, res) => {
         })
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+        
 
         const line_items = req.body.items.map((item) => ({
             price_data: {
