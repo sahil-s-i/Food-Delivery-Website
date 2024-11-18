348247ce5a95acdcb7a1773b809278ba9fdcff50
Sahil <sahilismailnavar@gmail.com>
2024-11-18 23:42:59 +0530

    Added code in update status func with try blck

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -98,6 +98,7 @@ const listOrders = async (req, res) => {
 const updateStatus = async (req, res) => {
     try {
         await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" });
     } catch (error) {
 
     }
