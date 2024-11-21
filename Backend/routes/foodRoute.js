b1cc1b7e5778fca81089b6eca0a6dca11bf64259
Sahil <sahilismailnavar@gmail.com>
2024-11-21 22:47:55 +0530

    Added comment lines in the code

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -5,7 +5,6 @@ import multer from "multer";
 const foodRouter = express.Router();
 
 // Image Storage Engine
-
 const storage = multer.diskStorage({
     destination: "uploads",
     filename: (req, file, cb) => {
